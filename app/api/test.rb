6cf9ff7095369747a26cf13992ed603a446e7b99
Yuanhao Sun <sunyuanhao123456@gmail.com>
2017-09-09 14:56:20 +0800

    Add test.rb

--- app/api/test.rb
+++ app/api/test.rb
@@ -3,9 +3,39 @@ module Api
     version 'v1', using: :param # Define API::Version
     format :json # Define return format
 
-    get :ping do
-      { data: "pong" }
+    params do
+      optional :password, type: String
     end
+
+    helpers do
+      def current_user_data(username, password)
+        username = username.to_s; password = password.to_s; temp = {}
+        page = Mechanize.new
+        page.get "http://seat.ujn.edu.cn/rest/auth?username=#{username}&password=#{password}"
+
+        data = JSON.parse(page.page.body)
+        status = data['status']
+        return { 'status': 'fail', 'data': nil } if status == 'fail'
+
+        token = data['data']['token']
+        page.get "http://seat.ujn.edu.cn/rest/v2/history/1/20?token=#{token}"
+        data = JSON.parse(page.page.body)
+
+        temp['status'] = data['status']; temp['data'] = data['data']['reservations']
+        temp
+      end
+    end
+
+    get '/user/:username/:password' do
+      if params[:password].present?
+        current_user_data(params[:username], params[:password])
+      end
+    end
+
+    get '/user/:username' do
+      current_user_data(params[:username], params[:username])
+    end
+
   end
 
 end
