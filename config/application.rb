70ace8a8e85f24a02a93b20460eea4cdb61a5a27
Yuanhao Sun <sunyuanhao123456@gmail.com>
2019-03-07 13:57:01 +0800

    <fix> 更新I18之后确保正确信息

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module LibrarySeatsApi
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.i18n.fallbacks = [I18n.default_locale]
 
     # Load grape api
     config.paths.add File.join('app', 'api'), glob: File.join('**', '*.rb')
