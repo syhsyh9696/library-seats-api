39b6265ee0f6ded431b6c4e5b3c4ad52a38169ad
Yuanhao Sun <sunyuanhao123456@gmail.com>
2019-03-08 16:34:16 +0800

    <fix> 不再使用open3库改用system
    不再使用open3是由于open3无法update_crontab

--- app/jobs/crontab_file_update_job.rb
+++ app/jobs/crontab_file_update_job.rb
@@ -1,9 +1,7 @@
-require 'open3'
-
 class CrontabFileUpdateJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
-    out, err, status = Open3.capture3('cd #{Rails.root} && whenever --update-crontab')
+    system('cd #{Rails.root} && whenever --update-crontab')
   end
 end
